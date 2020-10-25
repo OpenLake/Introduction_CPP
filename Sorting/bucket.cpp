c1ddbdcf3d5f6fea85839dbf7ae26ef07b0aad96
mohitv02 <mohitverma11s3@gmail.com>
2020-10-25 15:48:10 +0530

    bucket sort

--- Sorting/bucket.cpp
+++ Sorting/bucket.cpp
@@ -1,3 +1,4 @@
+
 #include "sorting.hpp"
 
 /**
@@ -7,6 +8,23 @@
  * 
  * Returns: null
  */
-void Sort::BucketSort(){
-    std::cout<<"[TODO @ Sorting/bucket.cpp] BucketSort() needs to be implemented.\n";
-}
+void bucketSort(std::vector<int>sorted, int n) 
+{ 
+  
+    std::vector<int>xsorted[n];
+  
+ 
+    for (int i = 0; i < n; i++) { 
+        int bi = n * arr[i]; 
+        xsorted[bi].push_back(arr[i]); 
+    } 
+
+    for (int i = 0; i < n; i++) 
+        sort(xsorted[i].begin(), xsorted[i].end()); 
+
+    int index = 0; 
+    for (int i = 0; i < n; i++) 
+        for (int j = 0; j < xsorted[i].size(); j++) 
+            arr[index++] = xsorted[i][j]; 
+} 
+  
