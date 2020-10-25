c42f87855e1bc2a2ddc204dfcc8fb86ab887f5ab
Samarth Singh Pawar <samarths@iitbhilai.ac.in>
2020-10-25 18:10:00 +0530

    QuickSort Implementation

--- Sorting/quick.cpp
+++ Sorting/quick.cpp
@@ -2,11 +2,44 @@
 
 /**
  * Sort the array using quick sort algortihm.
- * 
+ *
  * Inputs: null
- * 
+ *
  * Returns: null
  */
-void Sort::QuickSort(){
-    std::cout<<"[TODO @ Sorting/quick.cpp] QuickSort() needs to be implemented.\n";
+
+ //QUICK SORT ALGORITHM
+ int cnt{};
+ std::vector<int>arr;
+ void Swap(int &a,int &b){
+   int t=a;
+   a=b;
+   b=t;
+}
+
+int Partition(int p, int r){
+   int x = arr[r];
+   int i = p-1;
+   for(int j{p};j<r;++j){
+       if(arr[j]<=x){
+           i++;
+           Swap(arr[i], arr[j]);
+       }
+   }
+   Swap(arr[i+1], arr[r]);
+   return i+1;
+}
+
+void Sort::QuickSort(int p, int r){
+    if(!cnt){
+        r = unsorted.size()-1;
+        arr=unsorted;
+        cnt++;
+    }
+    if(p<r){
+        int q = Partition(p, r);
+        QuickSort(p, q-1);
+        QuickSort(q+1, r);
+    }
+    sorted = arr;
 }
