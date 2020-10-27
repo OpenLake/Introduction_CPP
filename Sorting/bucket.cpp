57aa10fc6b4182314af3f2441ed625e8133cfae4
mohitv02 <mohitverma11s3@gmail.com>
2020-10-27 16:58:11 +0530

    small changes in sorting.cpp

--- Sorting/bucket.cpp
+++ Sorting/bucket.cpp
@@ -1,30 +1,12 @@
-
 #include "sorting.hpp"
 
 /**
- * Sort the bucket sort algorithm.
+ * Sorts the array using bucket sort algorithm.
  * 
  * Inputs: null
  * 
  * Returns: null
  */
-void bucketSort(std::vector<int>sorted, int n) 
-{ 
-  
-    std::vector<int>xsorted[n];
-  
- 
-    for (int i = 0; i < n; i++) { 
-        int bi = n * arr[i]; 
-        xsorted[bi].push_back(arr[i]); 
-    } 
-
-    for (int i = 0; i < n; i++) 
-        sort(xsorted[i].begin(), xsorted[i].end()); 
-
-    int index = 0; 
-    for (int i = 0; i < n; i++) 
-        for (int j = 0; j < xsorted[i].size(); j++) 
-            arr[index++] = xsorted[i][j]; 
-} 
-  
+void Sort::BucketSort(){
+    std::cout<<"[TODO @ Sorting/bucket.cpp] BucketSort() needs to be implemented.\n";
+}
