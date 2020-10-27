a90ab9d9bd039f875122efa1af7719638506464a
jvmolu <anmol.singhal50@gmail.com>
2020-10-27 16:39:34 +0530

    merge sort updated. Removed global vector from merge.cpp

--- Sorting/merge.cpp
+++ Sorting/merge.cpp
@@ -5,6 +5,69 @@
  *
  * Inputs: null
  */
-void Sort::MergeSort(){
-    std::cout<<"[TODO @ Sorting/merge.cpp] MergeSort() needs to be implemented.\n";
-}
+ void Merge(vector<int> &sorted,int low, int mid, int high)
+ {
+     std::vector<int> L;
+     std::vector<int> R;
+
+     for(int i{low}; i<= mid; i++)
+     {
+         L.push_back(sorted[i]);
+     }
+
+     for(int i{mid + 1}; i<= high; i++)
+     {
+         R.push_back(sorted[i]);
+     }
+
+     int i{0};
+     int j{0};
+     int k{low};
+
+     while(i < L.size() && j < R.size())
+     {
+         if(L[i]  <= R[j])
+         {
+             sorted[k] = L[i];
+             i++;
+             k++;
+         }
+         else
+         {
+             sorted[k] = R[j];
+             j++;
+             k++;
+         }
+     }
+
+     while(j < R.size())
+     {
+         sorted[k] = R[j];
+         j++;
+         k++;
+     }
+
+     while(i < L.size())
+     {
+         sorted[k] = L[i];
+         i++;
+         k++;
+     }
+ }
+
+ void Sort::MergeSort(int low = 0, int high = -1)
+ {
+     if(high == -1)
+     {
+       sorted = unsorted;
+       high = unsorted.size() - 1;
+     }
+     if(high > low)
+     {
+         int mid = (low + high)/2;
+         MergeSort(low,mid);
+         MergeSort(mid+1,high);
+         Merge(sorted,low,mid,high);
+     }
+
+ }
